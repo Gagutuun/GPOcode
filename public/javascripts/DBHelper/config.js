e7beffcad55f7b10187e244c8dd0bbe582e5c9dd
Gydizu <nikita.alshevskiy@mail.ru>
2023-02-14 13:50:53 +0700

    наброски DBHelper'а

---
+++ public/javascripts/DBHelper/config.js
@@ -0,0 +1,5 @@
+const db_name = 'postgres';
+const userName = 'postgres';
+const password = '1111';
+const host = 'localhost';
+const dialect = 'postgres';
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 var express = require('express');
 var router = express.Router();
+// var DBHelper = require('../public/javascripts/DBHelper');
+// var dbHelper = new DBHelper();
+// dbHelper.testGetReq();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
