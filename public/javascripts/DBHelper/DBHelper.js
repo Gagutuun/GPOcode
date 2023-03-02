e3cd0d8810fbe76936244509ef00431cfdda36ad
Gydizu <nikita.alshevskiy@mail.ru>
2023-03-03 02:58:30 +0700

    Preparation for communication between the database and the server

--- public/javascripts/DBHelper/DBHelper.js
+++ public/javascripts/DBHelper/DBHelper.js
@@ -1,39 +1,12 @@
-const Sequelize = require("sequelize");
+const { Pool } = require('pg');
+const pool = new Pool({
+    host: 'localhost',
+    user: 'postgres',
+    max: 20,
+    password: 'admin',
+    database: 'bd'
+})
 
-class DBHelper {
-    #sequelize = new Sequelize(db_name, userName, password, {
-        host: host,
-        dialect: dialect,
-        define: {
-            timestamps: false
-        }
-    });
-    constructor() {
-        this.#testConnection();
-    }
-    async #testConnection() {
-        try {
-            await this.#sequelize.authenticate();
-            alert('Connection has been established successfully.');
-          } catch (error) {
-            alert('Unable to connect to the database:', error);
-          }
-    }
-    closeConnection() {
-        this.#sequelize.close();
-    }
-    testGetReq() {
-        const Test = this.#sequelize.define("Test", {
-            Name: {
-                type: Sequelize.TEXT
-            }
-        });
-        this.#sequelize.sync().then(result=>{
-            console.log(result);
-          })
-          .catch(err=> console.log(err));
-        Test.findAll({raw:true}).then(Test=>{
-            alert(Test);
-        }).catch(err=>alert(err));
-    }
+module.exports = {
+    
 }
--- public/javascripts/DBHelper/config.js
+++
@@ -1,5 +0,0 @@
-const db_name = 'postgres';
-const userName = 'postgres';
-const password = '1111';
-const host = 'localhost';
-const dialect = 'postgres';
