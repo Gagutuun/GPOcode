b879f40cd30e50a8e29f9328acb7c0947770be41
Gydizu <nikita.alshevskiy@mail.ru>
2023-03-23 09:22:37 +0700

    Изменения прошлой недели

--- app.js
+++ app.js
@@ -6,49 +6,12 @@ var logger = require('morgan');
 var fileUpload = require('express-fileupload');
 var officeParser = require('officeparser');
 var asyncHandler = require('express-async-handler')
-
+var parser = require('./parser.js');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var resultRouter = require('./routes/result');
 
 var app = express();
-
-const UPLINE = "РЕШИЛИ: ";
-const TIMETOEND = "Срок исполнения";
-const LASTRESH = "Решения предыдущих совещаний";
-const OTVETV = "Ответственн";
-const ZAMS = "заместители генерального директора";
-const DIRECTOR = "Бакало";
-
-
-function Finder(data) {
-  let regPoruch = new RegExp("\d{1,}[.]");
-  let parsedData = new Array();//Матрица данных, которые отправляем в БД
-  data = data.replace(data.substr(0, data.indexOf(UPLINE) + 8 ), ""); //Убираем все до "РЕШИЛИ: " включительно. 
-  console.log(data);
-  console.log();
-  while(data.indexOf(TIMETOEND) != -1) {
-      while(data.charAt(0)== ' ')
-          data = data.replace(" ","");
-      if(data.indexOf(LASTRESH) == 0)
-          data = data.replace(LASTRESH + ' ',"");
-      substrOfData = data.slice(0, data.indexOf(TIMETOEND) + 29);//Вырезаем подстроку для обработки
-      data = data.replace(substrOfData, "");//И сразу же удаляем ее
-      NameOfAsgmnt = substrOfData.slice(substrOfData.indexOf(OTVETV)+16, substrOfData.indexOf("Срок")); // Выделяем имена ответственных
-      if(NameOfAsgmnt.indexOf(DIRECTOR)!=-1||NameOfAsgmnt.indexOf(ZAMS)!=-1){ // Проверяем на наличие нужных имён
-          DateOfExe = substrOfData.slice(substrOfData.indexOf(TIMETOEND + " – ") + 18, substrOfData.indexOf(". П")); // Выделяем дату поручения
-          TextOfPoruch = substrOfData.slice(substrOfData.search(regPoruch) + regPoruch.length, substrOfData.indexOf(". " + OTVETV)); // Выделяем текст поручения
-      } else continue;
-      console.log(substrOfData);
-      //ReqToBase();
-      data=data.replace(substrOfData, "");
-      substrOfData = null;
-      parsedData.push(new Array(TextOfPoruch, NameOfAsgmnt, DateOfExe));
-  }
-  return parsedData;
-}
-
-
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -62,9 +25,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+let uploadPath;
 app.post('/upload', function(req, res, next) {
   let sampleFile;
-  let uploadPath;
 
   if (!req.files || Object.keys(req.files).length === 0) {
     res.status(400).send('No files were uploaded.');
@@ -95,10 +59,10 @@ app.post(
         setTimeout(resolve, ms);
   });
 }
-    await officeParser.parseWordAsync(/* PATH */"public/files/protokol.docx")
+    await officeParser.parseWordAsync(uploadPath)
     .then((data) => {
 
-      res.render('result', { title: 'GPO_test', text: data , result: Finder(data)});
+      res.render('result', { title: 'GPO_test', text: data , result: parser.Finder(data)});
         });
   })
 )
---
+++ parser.js
@@ -0,0 +1,36 @@
+const UPLINE = "РЕШИЛИ: ";
+const TIMETOEND = "Срок исполнения";
+const LASTRESH = "Решения предыдущих совещаний";
+const OTVETV = "Ответственн";
+const ZAMS = "заместители генерального директора";
+const DIRECTOR = "Бакало";
+const STOP_WORD = "Решение предыдущих совещаний";
+
+function Finder(data) {
+    let regPoruch = new RegExp("\d{1,}[.]");
+    let parsedData = new Array();//Матрица данных, которые отправляем в БД
+    data = data.replace(data.substr(0, data.indexOf(UPLINE) + 8 ), ""); //Убираем все до "РЕШИЛИ: " включительно. 
+    console.log(data);
+    console.log();
+    while(data.indexOf(TIMETOEND) != -1 || data.indexOf(STOP_WORD) != 0) {
+        while(data.charAt(0)== ' ')
+            data = data.replace(" ","");
+        if(data.indexOf(LASTRESH) == 0)
+            data = data.replace(LASTRESH + ' ',"");
+        substrOfData = data.slice(0, data.indexOf(TIMETOEND) + 29);//Вырезаем подстроку для обработки
+        data = data.replace(substrOfData, "");//И сразу же удаляем ее
+        NameOfAsgmnt = substrOfData.slice(substrOfData.indexOf(OTVETV)+16, substrOfData.indexOf("Срок")); // Выделяем имена ответственных
+        if(NameOfAsgmnt.indexOf(DIRECTOR)!=-1||NameOfAsgmnt.indexOf(ZAMS)!=-1){ // Проверяем на наличие нужных имён
+            DateOfExe = substrOfData.slice(substrOfData.indexOf(TIMETOEND + " – ") + 18, substrOfData.indexOf(". П")); // Выделяем дату поручения
+            TextOfPoruch = substrOfData.slice(substrOfData.search(regPoruch) + regPoruch.length, substrOfData.indexOf(". " + OTVETV)); // Выделяем текст поручения
+        } else continue;
+        console.log(substrOfData);
+        //ReqToBase();
+        data=data.replace(substrOfData, "");
+        substrOfData = null;
+        parsedData.push(new Array(TextOfPoruch, NameOfAsgmnt, DateOfExe));
+    }
+    return parsedData;
+  }
+
+module.exports = {Finder};
