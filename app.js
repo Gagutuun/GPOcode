2b8df6488db66c633b2282fb558cdf2a568cf80d
Ilya Atroshenko <fanandgame@gmail.com>
2023-04-06 15:17:39 +0700

    мелкие исправления

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const logger = require('morgan');
 const fileUpload = require('express-fileupload');
 const session = require('express-session');
 const passport = require('./src/config/authConfig');
+const flash = require('connect-flash');
 // -----------------------------------------------------
 const router = require('./src/routes/index');
 
@@ -20,6 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'src/public')));
+app.use(flash())
 app.use(session({
   secret: 'verycherry',
   resave: false,
@@ -27,9 +29,7 @@ app.use(session({
 }));
 app.use(passport.initialize());
 app.use(passport.session());
-console.log("Я тут: app.use('/', router);");
 app.use('/', router);
-
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
