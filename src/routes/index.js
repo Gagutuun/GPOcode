d91957451996e68dd5c10da6a0f14dff6c5c94b1
Ilya Atroshenko <fanandgame@gmail.com>
2023-04-06 21:50:43 +0700

    Обновил страницу авторизации. Теперь красиво.

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,11 +4,13 @@ const router = express.Router();
 const mainRouter = require('./mainRouter');
 const uploadRouter = require('./uploadRouter');
 const authRouter = require('./authRouter');
+const testRouter = require('./testRouter');
 
 // Use routes
 router.use('/', mainRouter);
 router.use('/upload', uploadRouter);
 router.use('/auth', authRouter);
+router.use('/test', testRouter);
 
 
 module.exports = router;
