c7b7b7ea3e5f98d6e2329ac0f01753e9fb14cfc7
Gydizu <nikita.alshevskiy@mail.ru>
2023-04-08 13:01:07 +0700

    да

--- src/models/protocol.js
+++ src/models/protocol.js
@@ -13,8 +13,6 @@ const columnNames = {
 class Protocol {
     static addNewProtocol(protocolPath/*, protocolDate, protocolNumber*/) {
         db.query(
-            // 'INSERT INTO public."Protocol" (file_protocol_doc) VALUES ($1)',
-            // [protocolPath],
             makeInsertQuery(tableName, new Array(columnNames.file_protocol_doc), 1),
             [protocolPath],
             (err, res) => {
