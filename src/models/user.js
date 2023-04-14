594fff9cbbba0815dbac7ee5a4c48b3247fa7289
Gydizu <nikita.alshevskiy@mail.ru>
2023-04-14 13:22:14 +0700

    ну потихоньку да

--- src/models/user.js
+++ src/models/user.js
@@ -1,21 +1,38 @@
 const db = require('../config/dbConfig');
+const { makeSelectQuery } = require('../utils/queryBuilder');
 
+const tableName = 'public."Employee"';
+const columnNames = {
+  id: 'id',
+  surname: 'surname',
+  name: 'name',
+  patronymic: 'patronymic',
+  login: 'login',
+  password: 'password',
+  position: 'position',
+  role: 'role',
+  category: 'category',
+  subdivisionShortName: 'subdivision_short_name'
+}
 
 class User {
   // Изменим название метода, чтобы оно точнее отражало его назначение
   static findByLoginAndPassword(login, password) {
     return new Promise((resolve, reject) => {
-      db.query('SELECT * FROM public."Employee" WHERE login = $1 AND password = $2', [login, password], (error, result) => {
-        if (error) {
-          // Заменим console.log на reject, чтобы можно было обработать ошибку в вызывающем коде
-          reject(error);
-        } else if (result.rows.length > 0) {
-          const {id, login, password} = result.rows[0]; // Деструктурируем объект
-          resolve({ id, login, password }); // Объект можно создать короче
-        } else {
-          resolve(null);
-        }
-      });
+      // db.query('SELECT * FROM public."Employee" WHERE login = $1 AND password = $2', [login, password], (error, result) => { 
+      //   if (error) {
+      //     // Заменим console.log на reject, чтобы можно было обработать ошибку в вызывающем коде
+      //     reject(error);
+      //   } else if (result.rows.length > 0) {
+      //     const {id, login, password} = result.rows[0]; // Деструктурируем объект
+      //     resolve({ id, login, password }); // Объект можно создать короче
+      //   } else {
+      //     resolve(null);
+      //   }
+      // });
+      db.query(
+        makeSelectQuery(null, tableName, )
+      )
     });
   }
 
