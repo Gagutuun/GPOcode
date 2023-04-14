8fcda3e83e35328445578948b397c8ff9416df06
Gydizu <nikita.alshevskiy@mail.ru>
2023-04-14 12:21:36 +0700

    небольшие комментарии для модели

--- src/models/errand.js
+++ src/models/errand.js
@@ -1,22 +1,27 @@
 // Модель поручения
 const db = require('../config/dbConfig');
+const { makeInsertQuery } = require('../utils/queryBuilder');
 
 const tableName = 'public."Errand"';
 const columnNames = {
     id: 'id',
-    id_responsible: 'id_responsible',
-    text_errand: 'text_errand',
-    constantly: 'constantly',
-    short_report_doc: 'short_report_doc',
-    scheduled_due_date: 'scheduled_due_date',
-    actual_date: 'actual_date',
-    status: 'status',
-    id_protocol: 'id_protocol'
+    id_responsible: 'id_responsible', // Данные для вставки: idResponsible
+    text_errand: 'text_errand', // Данные для вставки: errandParsedData.errandText
+    constantly: 'constantly', // Ставится, если errandParsedData.deadline = "постоянно";
+    short_report_doc: 'short_report_doc', // Заполняется позже
+    scheduled_due_date: 'scheduled_due_date', // Ставится, если errandParsedData.deadline - дата. 
+    actual_date: 'actual_date', // Ставится позже
+    status: 'status', // Я хз че это
+    id_protocol: 'id_protocol' // Нужно получить из запроса к протоколу
 }
 
 class Errand {
     static addNewErrand(errandParsedData) {
         // Выполняем запрос к таблице сотрудников на получение id ответсенного.
+        const idResponsible = 1;
+        db.query(
+            
+        )
         // Создаем запрос на добавление новой записи в таблицу Errand
     }
 }
