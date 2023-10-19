8a828bb247503f7909a5e73483b264a9c69ee134
Gydizu <nikita.alshevskiy@mail.ru>
2023-10-19 12:19:03 +0700

    Комментарии к коду

--- test.js
+++ test.js
@@ -1,13 +1,33 @@
 const { exec } = require('child_process');
 const iconv = require('iconv-lite');
 
+/**
+ * Кодировка cp866
+ */
 const cp866 = 'cp866';
+/**
+ * Кодировка utf-8
+ */
 const utf8 = 'utf-8';
 
+/**
+ * Метка для нахождения локальных групп
+ */
 const LOCAL_GROUPS_MARK = "Членство в локальных группах";
+/**
+ * Метка на нахождение глобальных групп
+ */
 const GLOBAL_GROUPS_MARK = "Членство в глобальных группах";
+/**
+ * Метка "успешного завершения команды" (идет после глобальных групп)
+ */
 const SUCCESS_MESSAGE = "Команда выполнена успешно.";
 
+/**
+ * Возвращает системную информацию о пользователе
+ * @param {string} username - имя пользователя
+ * @returns Промис на результирующую строку выполнения команды "net user USER_NAME"
+ */
 function getUserInfo(username) {
     return new Promise((resolve, reject) => {
         const user_info = `net user ${username}`;
@@ -25,6 +45,13 @@ function getUserInfo(username) {
     })
 }
 
+/**
+ * Возвращает массив групп, к которым принадлежит пользователь
+ * @param {string} userInfo - информация о пользователе
+ * @param {string} start - начальная метка
+ * @param {string} end - конечная метка
+ * @returns Промис на возвращение массива групп
+ */
 function _getGroups(userInfo, start, end) {
     return new Promise((resolve, reject) => {
         try {
@@ -40,10 +67,20 @@ function _getGroups(userInfo, start, end) {
     });
 }
 
+/**
+ * Возвращает все локальные группы, к которым принадлежит пользователь
+ * @param {string} userInfo - информация о пользователе
+ * @returns Промис на массив групп
+ */
 function getLocalGroups(userInfo) {
     return _getGroups(userInfo, LOCAL_GROUPS_MARK, GLOBAL_GROUPS_MARK);
 }
 
+/**
+ * Возвращает все глобальные группы, к которым принадлежит пользователь
+ * @param {string} userInfo - информация о пользователе
+ * @returns Промис на массив групп
+ */
 function getGlobalGroups(userInfo) {
     return _getGroups(userInfo, GLOBAL_GROUPS_MARK, SUCCESS_MESSAGE);
 }
