710102a21ff6dd1f2b4c47036349829f085310bc
Gydizu <nikita.alshevskiy@mail.ru>
2023-10-19 11:33:55 +0700

    Модернизировал под легкое интегрирование в систему

--- test.js
+++ test.js
@@ -1,17 +1,33 @@
 const os = require('os');
 const { exec } = require('child_process');
 const iconv = require('iconv-lite');
+
 const cp866 = 'cp866';
 const utf8 = 'utf-8';
-const user_info = `net user ${os.userInfo().username}`;
 
-exec(user_info, { encoding: cp866 }, (err, stdout, stderr) => {
-    if (err) {
+function getUserInfo(username) {
+    return new Promise((resolve, reject) => {
+        const user_info = `net user ${username}`;
+        exec(user_info, { encoding: cp866 }, (err, stdout, stderr) => {
+            if (err) {
+                reject(err);
+                return;
+            }
+            if (stdout.length != 0) {
+                resolve(iconv.decode(stdout, cp866).toString(utf8));
+                return;
+            }
+            reject(iconv.decode(stderr, cp866).toString(utf8));
+        })
+    })
+}
+
+module.exports = { getUserInfo };
+
+getUserInfo(os.userInfo().username)
+    .then((res) => {
+        console.log(res);
+    })
+    .catch((err) => {
         console.error(err);
-        return;
-    }
-    if (stdout.length != 0)
-        console.log(iconv.decode(stdout, cp866).toString(utf8));
-    else
-        console.log(iconv.decode(stderr, cp866).toString(utf8));
-})
+    })
